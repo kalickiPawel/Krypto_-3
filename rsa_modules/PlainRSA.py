8a14bd4f45684a41b5af8656848ccd0daf5cafd4
Paweł Kalicki <kalicki.paul@icloud.com>
2020-04-27 15:10:04 +0200

    Changed assignments

--- rsa_modules/PlainRSA.py
+++ rsa_modules/PlainRSA.py
@@ -11,7 +11,8 @@ class PlainRSA():
         self.size = size
         p, q = getPrime(size), getPrime(size)
         phi = (p-1)*(q-1)
-        self.n, self.e, self.d = p*q, 65537, self.modinv(self.e, phi)
+        self.n, self.e = p*q, 65537
+        self.d = self.modinv(self.e, phi)
 
     def egcd(self, a, b):
         if a == 0:
