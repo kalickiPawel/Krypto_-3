ebe9e5435d111e8b1f7bb9a7bcbcd44ee2ca1d0f
Paweł Kalicki <kalicki.paul@icloud.com>
2020-04-29 01:25:18 +0200

    Added the expected return type of the method validation

--- experiments/Experiment.py
+++ experiments/Experiment.py
@@ -8,7 +8,7 @@ class Experiment(PlainRSA):
     def __init__(self, size):
         super().__init__(size)
 
-    def validation(self, s, m):
+    def validation(self, c, m) -> bool:
         logger.warn("Start validation")
         decrypt = self.decryptMessage(s)
         return m == decrypt
