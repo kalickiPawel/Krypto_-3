1890246c011bddb6edce5a2af6cd72b1705ac0d0
Paweł Kalicki <kalicki.paul@icloud.com>
2020-04-29 12:20:19 +0200

    Added main Class

--- main.py
+++ main.py
@@ -6,13 +6,77 @@ securityLength = 128
 logger = logging.getLogger(__name__)
 
 
-print('Klucz publiczny: {}'.format(obj_rsa.e))
-print('Klucz prywatny: {}\n'.format(obj_rsa.d))
+class Main:
+    def __init__(self, select_obj):
+        self.select = int(select_obj)
+        logger.info("Initialized base program")
+        self.get_selection()
 
-print('poczatkowa wiadomosc: {}'.format(message))
-print('zaszyfrowany int tekstu: {}'.format(encrypted_int))
-print('odszyfrowany int tekstu: {}'.format(decrypted_int))
-print('koncowa wiadomosc: {}\n'.format(decrypted_text))
+    def get_selection(self):
+        logger.warning("Choice: {}".format(self.select))
+        if self.select == 0:
+            self.generate_plain_rsa()
+        if self.select == 1:
+            self.generate_fdh_rsa()
+        if self.select == 2:
+            self.generate_exp1()
+
+    @staticmethod
+    def generate_plain_rsa():
+        message = 'Hello Bob!'
+        obj_rsa = PlainRSA(securityLength)
+        int_text = obj_rsa.generate_int(message)
+        encrypted_int = obj_rsa.encrypt_message(int_text)
+        decrypted_int = obj_rsa.decrypt_message(encrypted_int)
+        decrypted_text = obj_rsa.generate_txt(decrypted_int)
+
+        print('Klucz publiczny: {}'.format(obj_rsa.e))
+        print('Klucz prywatny: {}\n'.format(obj_rsa.d))
+
+        print('poczatkowa wiadomosc: {}'.format(message))
+        print('zaszyfrowany int tekstu: {}'.format(encrypted_int))
+        print('odszyfrowany int tekstu: {}'.format(decrypted_int))
+        print('koncowa wiadomosc: {}\n'.format(decrypted_text))
+
+    @staticmethod
+    def generate_fdh_rsa():
+        obj_fdh_rsa = FdhRSA(securityLength)
+        message = "To jest test dla FDH RSA"
+        print('\nImplementacja FDH RSA\npoczatkowa wiadomosc: {}'.format(message))
+        encrypted_int = obj_fdh_rsa.encrypt_message(message)
+        decrypted_int = obj_fdh_rsa.decrypt_message(encrypted_int)
+        print('Czy poprawne szyfrowanie?: {}'.format(
+            'Tak' if obj_fdh_rsa.validation(encrypted_int, decrypted_int) else 'Nie'))
+
+    @staticmethod
+    def generate_exp1():
+        obj_exp_1 = Exp1(securityLength)
+        (s, m) = obj_exp_1.no_message_attack()
+        print('Wygenerowany podpis: {}'.format(s))
+        print('Zaszyfrowany int tekstu: {}'.format(m))
+        print('Czy poprawne szyfrowanie?: {}\n'.format(
+            'Tak' if obj_exp_1.validation() else 'Nie'))
+
+    @staticmethod
+    def generate_exp2():
+        message1 = "Za oknem pada deszcz"
+        message2 = "Moj tajny tekst."
+        obj_exp_2 = Exp2(securityLength, message1, message2)
+        s = obj_exp_2.make_experiment()
+        print('Wygenerowany podpis: {}'.format(s))
+        print('Czy poprawne szyfrowanie?: {}\n'.format(
+            'Tak' if obj_exp_2.validation() else 'Nie'))
+
+    @staticmethod
+    def generate_exp3():
+        obj_exp_3 = Exp3(securityLength)
+        print('Odczytana wiadomość to: {}\n'.format(obj_exp_3.make_experiment))
+
+    @staticmethod
+    def generate_exp4():
+        message = "Przykladowy tekst Håstad"
+        obj_exp_4 = Exp4(securityLength, message)
+        print('Odczytana wiadomosc: {}'.format(obj_exp_4.make_experiment()))
 
 
 def generate_selection():
